13106492f17ce65b0c8827aca35e4914bcd8f6f3
TiagoGashu <tiago.gashu@gmail.com>
2019-12-27 13:02:58 -0300

    Ex. 4.6 - MyEither impl

--- src/main/scala/com/gashu/myfpinscala/chapter4/MyEither.scala
+++ src/main/scala/com/gashu/myfpinscala/chapter4/MyEither.scala
@@ -5,13 +5,33 @@ package com.gashu.myfpinscala.chapter4
  **/
 sealed trait MyEither[+E, +A] {
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): MyEither[E, B] =
+    this match {
+      case Right(value) => Right(f(value))
+      case Left(e) => Left(e)
+    }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => MyEither[EE, B]): MyEither[EE, B] =
+    this match {
+      case Right(value) => f(value)
+      case Left(e) => Left(e)
+    }
 
-  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E,B >: A](b: => MyEither[EE, B]): MyEither[EE, B] =
+    this match {
+      case Right(v) => Right(v)
+      case Left(_) => b
+    }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C):Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: MyEither[EE, B])(f: (A, B) => C):MyEither[EE, C] =
+    this match {
+      case Right(v) =>
+        b match {
+          case Right(bValue) => Right(f(v, bValue))
+          case Left(bError) => Left(bError)
+        }
+      case Left(e) => Left(e)
+    }
 
 }
 
