c0ea46c1bab5437311b1e65ecfdf94f2b19543b1
TiagoGashu <tiago.gashu@gmail.com>
2020-01-12 11:39:01 -0300

    Chapter 7 - non-blocking impl with actor

--- src/main/scala/com/gashu/myfpinscala/chapter7/Par.scala
+++ src/main/scala/com/gashu/myfpinscala/chapter7/Par.scala
@@ -1,6 +1,9 @@
 package com.gashu.myfpinscala.chapter7
 
-import java.util.concurrent.{Callable, ExecutorService, Future}
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
+
+import com.gashu.myfpinscala.parallelism.Actor
 
 import scala.concurrent.duration.TimeUnit
 
@@ -14,27 +17,59 @@ private case class UnitFuture[A](get: A) extends Future[A] {
   def cancel(evenIfRunning: Boolean): Boolean = false
 }
 
+sealed trait Future[A] {
+  private[chapter7] def apply(k: A => Unit): Unit
+}
+
 object Par {
 
   type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
+  def unit[A](a: A): Par[A] =
+    _ => new Future[A] {
+      def apply(cb: A => Unit): Unit =
+        cb(a)
+    }
 
   def fork[A](a: => Par[A]): Par[A] =
-    es => es.submit(new Callable[A] {
-      def call: A = a(es).get
-    })
+    es => new Future[A] {
+      def apply(cb: A => Unit): Unit =
+        eval(es)(a(es)(cb))
+    }
+
+  def eval(es: ExecutorService)(r: => Unit): Unit =
+    es.submit(new Callable[Unit] { def call = r })
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
-
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
-  (es: ExecutorService) => {
-    val af: Future[A] = a(es)
-    val bf: Future[B] = b(es)
-    UnitFuture(f(af.get, bf.get))
+  def run[A](es: ExecutorService)(p: Par[A]): A = {
+    val ref = new AtomicReference[A]
+    val latch = new CountDownLatch(1)
+    p(es) {
+      a => ref.set(a); latch.countDown()
+    }
+    ref.get
   }
+
+  def map2[A,B,C](p: Par[A], p2: Par[B])(f: (A,B) => C): Par[C] =
+    es => new Future[C] {
+      def apply(cb: C => Unit): Unit = {
+        var ar: Option[A] = None
+        var br: Option[B] = None
+        val combiner = Actor[Either[A,B]](es) {
+          case Left(a) => br match {
+            case None => ar = Some(a)
+            case Some(b) => eval(es)(cb(f(a, b)))
+          }
+          case Right(b) => ar match {
+            case None => br = Some(b)
+            case Some(a) => eval(es)(cb(f(a, b)))
+          }
+        }
+        p(es)(a => combiner ! Left(a))
+        p2(es)(b => combiner ! Right(b))
+      }
+    }
 
   // 7.4
   def asyncF[A, B](f: A => B): A => Par[B] =
@@ -67,4 +102,18 @@ object Par {
     })
 
   }
+
+  // TODO: generalize the sum fn
+
+  /**
+   * TODO:
+   *
+   * Write a function that takes a list of paragraphs (a List[String] ) and returns
+   *  the total number of words across all paragraphs, in parallel. Generalize this
+   *  function as much as possible.
+   *
+   */
+
+  // TODO: Implement map3 , map4 , and map5 , in terms of map2.
+
 }
